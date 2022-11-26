0b1fce232e25fa05a44730e2676d5c317db45d0b
Fernando Ferreira <fernando.ferreira@grancursosonline.com.br>
2022-11-25 21:59:49 -0300

    refactor: :fire: remove unused class

--- lib/src/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Platform {
-  static bool isIos(BuildContext context) => Theme.of(context).platform == TargetPlatform.iOS;
-  static bool isAndroid(BuildContext context) => Theme.of(context).platform == TargetPlatform.android;
-}
